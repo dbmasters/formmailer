564f9ed714175f82633bb6d6487e943a8c1b457f
Dan <danball@gmail.com>
2021-04-03 07:18:44 -0500

    Update formmailer.php

--- formmailer.php
+++ formmailer.php
@@ -3,8 +3,6 @@
 // dB Masters' PHP FormM@iler, Copyright (c) 2021 dB Masters Multimedia
 // http://www.dbmasters.net/
 // FormMailer comes with ABSOLUTELY NO WARRANTY
-// Licensed under the AGPL
-// See license.txt and readme.txt for details
 ////////////////////////////////////////////////////////////////////////////
 // General Variables
 	$check_referrer="no";
